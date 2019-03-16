6b9f9c38dc82de00b77d486b99c4a5db5b4fc5a9
Bearsie <wojtek.niedzwiedz@gmail.com>
2019-03-16 16:59:14 +0100

    Adds server.js to serving static website

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('docs'));
+
+  app.get('*', (_req, res) => {
+    res.sendFile(path.resolve(__dirname, 'docs', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
